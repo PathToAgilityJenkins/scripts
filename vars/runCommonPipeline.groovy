2c6b77b430ebb2bde51240687e80726bf9651457
Fox <kfox@ICC10308.local>
2017-05-23 22:39:41 -0400

    Add script for common pipeline flow

---
+++ vars/runCommonPipeline.groovy
@@ -0,0 +1,33 @@
+def call(String project)
+{
+	node
+	{
+		echo 'Running common pipeline for ${project}.'
+		
+		stage('Checkout')
+		{
+			checkout scm
+		}
+		
+		stage('Build')
+		{
+			dir(project)
+			{
+				withMaven(maven: 'Maven 3.5.0')
+				{
+					sh mvn package
+				}
+			}
+		}
+		
+		stage('Deploy')
+		{
+			echo 'Do something useful to deploy ${project}.'
+		}
+		
+		stage('Test')
+		{
+			echo 'Do something useful to test ${project}.'
+		}
+	}
+}
